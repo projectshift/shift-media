f2154f299851721de3fbf599282d8edcfb1702df
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2016-09-03 13:08:38 +0100

    Created default extendable config

---
+++ shiftmedia/config/__init__.py
@@ -0,0 +1,1 @@
+from shiftmedia.config.default import DefaultConfig
---
+++ shiftmedia/config/default.py
@@ -0,0 +1,9 @@
+
+class DefaultConfig():
+    """
+    Default media storage config
+    This contains all possible configuration values and sensible defaults.
+    Extend this class with your own local config class and override required
+    settings, for example security credentials.
+    """
+    pass
