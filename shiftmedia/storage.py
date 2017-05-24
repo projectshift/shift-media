988bf220c871d875703eaad3ed6398beb8b4ebeb
Dmitry Belyakov <dmitrybelyakov@users.noreply.github.com>
2017-05-24 11:20:02 +0100

    Update storage.py

--- shiftmedia/storage.py
+++ shiftmedia/storage.py
@@ -126,7 +126,7 @@ class Storage:
             format=params['output_format'],
             quality=params['quality']
         )
-        self.backend.put_variant(resize, id, filename)
+        self.backend.put_variant(resize, id, filename, force=True)
         os.remove(local_original)
         os.remove(resize)
         tmp_dir = os.path.join(self._tmp_path, id)
