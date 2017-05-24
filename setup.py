7b756152b089d858e231332a89405a71d7a111df
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2017-05-24 11:22:26 +0100

    Maintenance release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 # ----------------------------------------------------------------------------
 
 # project version
-version='0.0.3'
+version='0.0.4'
 
 # development status
 # dev_status = '1 - Planning'
